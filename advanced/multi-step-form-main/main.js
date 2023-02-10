5757689841a63f22ab9c8560cb1b012b91c680a0
sfwnisme <sfwnisme@gmail.com>
2023-02-10 09:26:10 +0300

    .

--- advanced/multi-step-form-main/main.js
+++ advanced/multi-step-form-main/main.js
@@ -26,3 +26,19 @@ pickBox.forEach((pick) => {
     }
   });
 });
+pickCheck.forEach((check) => {
+  check.addEventListener("click", (ch) => {
+    // ch.currentTarget.removeAttribute("checked");
+    // ch.currentTarget.checked = !ch.currentTarget.checked;
+    console.log("rvemo");
+    let confirm = ch.currentTarget.parentElement.parentElement;
+    console.log(confirm);
+    if (confirm.classList.contains("active-pick")) {
+      ch.currentTarget.checked = !ch.currentTarget.checked;
+    } else {
+      ch.currentTarget.checked;
+    }
+  });
+  // check.stopPropagation();
+  // check.preventDefault();
+});
